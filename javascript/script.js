53a9b3ef722a6e543d5818552f36218e0cfb7439
Pierre Bergue <pierre.bergue@gmail.com>
2020-12-11 12:11:11 +0100

    added hover effects and separating lines

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,20 @@ function copyToClipboard(element) {
   document.execCommand("copy");
   $temp.remove();
 }
+
+const addRedClass = () => {
+  const bgDiv = document.getElementById('background');
+  bgDiv.addEventListener('mouseover', (event) => {
+    const highlightTexts = document.querySelectorAll('.target-red');
+    highlightTexts.forEach((text) => {
+      text.classList.add('red-color');
+    });
+  });
+
+  bgDiv.addEventListener('mouseout', (event) => {
+    const highlightTexts = document.querySelectorAll('.target-red');
+    highlightTexts.forEach((text) => {
+      text.classList.remove('red-color');
+    });
+  });
+};
