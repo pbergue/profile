14dda78282656437b96803585d03b323d41033db
Pierre Bergue <pierre.bergue@gmail.com>
2020-12-13 13:11:12 +0100

    tried to implement clipboard on email

--- javascript/script.js
+++ javascript/script.js
@@ -26,3 +26,40 @@ const addRedClass = () => {
     });
   });
 };
+
+const emailCopy = () => {
+  const email = document.getElementById('email-icon');
+  email.addEventListener('click', (event) => {
+    /* Get the text field */
+    var copyText = document.querySelector('.hidden');
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+
+  });
+};
+
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementsByClassName("hidden")[0].innerText;
+  console.log(copyText);
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  copyText.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+emailCopy();
